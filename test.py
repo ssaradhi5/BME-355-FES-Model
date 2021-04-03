ed5459c9d13ce2e2e9e65644bc7c93887e037359
hvijayag <hvijayag@uwaterloo.ca>
2021-04-03 17:10:41 -0400

    input parameters

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
- import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
 
 offset_time=10
-slope=2
-max_value=5
-step_size=0.1
+offset_time=int(offset_time/2)
+slope=0.1
+max_value=1
+step_size=1
 time_tomax=max_value/slope
 
-times=np.arange(0,offset_time+(time_tomax*2)+step_size,step_size)
-output=np.zeros((times.shape))
 
 
 
@@ -34,6 +34,10 @@ offset_time_adjusted=offset_time/step_size
 
 time_tomax=int(time_tomax/step_size)
 offset_time=int(offset_time/step_size)
+
+times=np.arange(0,offset_time_adjusted+(time_tomax*2)+step_size,step_size)
+output=np.zeros((times.shape))
+
 # print(time_tomax)
 
 slope_array=np.arange(0,max_value,slope*step_size)
@@ -43,6 +47,9 @@ print(slope_array)
 
 output[0:time_tomax]=slope_array
 
-output[time_tomax: offset_time + time_tomax]= max_value
+output[time_tomax: (offset_time + time_tomax)]= max_value
 
 output[offset_time + time_tomax : -1]=decrease_slope
+
+plt.plot(output)
+plt.show()
